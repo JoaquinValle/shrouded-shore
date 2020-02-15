ce4b9d9968021f116ce62b1c30be859d99a6f092
Santiago MVO <santiago.mvao@gmail.com>
2020-02-15 13:50:05 -0600

    added user controller

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+const db = require("../models");
+
+// Defining methods for the userController
+module.exports = {
+  findOne: function(req, res) {
+    db.User
+    .findById({ _id: req.params.id })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findAll: function(req, res) {
+    db.User
+      .find(req.query)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.User
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.User
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
