cec3000ccb2c29d3f369ca08c9958f043e1674e5
Santiago MVO <santiago.mvao@gmail.com>
2020-02-18 20:38:09 -0600

    added simple api call to BGA

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,5 +4,9 @@ export default {
   // Get user by id
   getUser: function(id) {
     return axios.get("/api/user/" + id);
+  },
+  
+  BGA:function(gameName){
+    return axios.get(`https://www.boardgameatlas.com/api/search?name=${gameName}&pretty=true&client_id=SB1VGnDv7M`);
   }
 };
