6918fc32defd658d627ea8c31ce3358dd62a2cf8
Santiago MVO <santiago.mvao@gmail.com>
2020-02-22 13:12:07 -0600

    changed sidenav appearance

--- client/src/components/Sidenav/index.js
+++ client/src/components/Sidenav/index.js
@@ -10,8 +10,20 @@ function Nav() {
   return (
     <aside>
       <ul id="slide-out" className="sidenav sidenav-fixed sidenavWrapper">
-        <li><a className="flow-text" href="#!">User</a></li>
-        <li><a href="#!">Games</a></li>
+
+        <li><div class="user-view">
+          <div class="background cyan darken-4"></div>
+          <a href="#user"><img class="circle" src="https://randomuser.me/api/portraits/men/72.jpg"/></a>
+          <a href="#name"><span class="white-text name">John Doe</span></a>
+          <a href="#email"><span class="white-text email">dohn_joe@gmail.com</span></a>
+        </div></li>
+
+        <li><a className="waves-effect" href="#!"><i class="material-icons">account_circle</i>Profile</a></li>
+
+        <li><a className="waves-effect" href="#!"><i class="material-icons">collections</i>Games</a></li>
+
+        <li><a className="waves-effect" href="#!"><i class="material-icons">contacts</i>Friends</a></li>
+
       </ul>
     </aside>
   );
