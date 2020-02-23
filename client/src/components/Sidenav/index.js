7ea2bd70065b32a78792ce579e82ff2482a07e6c
Santiago MVO <santiago.mvao@gmail.com>
2020-02-22 19:05:05 -0600

    working on login sidenav animation

--- client/src/components/Sidenav/index.js
+++ client/src/components/Sidenav/index.js
@@ -3,7 +3,8 @@ import "./style.css";
 import M from 'materialize-css';
 
 function Nav(props) {
-  const [userState, setUserState]=useState(false);
+  const [userState, setUserState] = useState(false);
+  const [animState, setAnimState] = useState("");
 
   useEffect(() => {
     M.Sidenav.init(document.querySelectorAll('#sideNav'),{menuWidth:170});
@@ -11,7 +12,7 @@ function Nav(props) {
 
   return (
     <aside>
-      <ul id="sideNav" className="sidenav sidenav-fixed sidenavWrapper">
+      <ul id="sideNav" className={"sidenav sidenav-fixed sidenavWrapper "+animState}>
         <li><div className="user-view">
           <div className="background cyan darken-4"></div>
           {userState?(<div>
@@ -19,14 +20,14 @@ function Nav(props) {
             <a href="#profile"><span className="white-text name">Dohn Joe</span></a>
             <a href="#logout" onClick={()=>setUserState(false)}><span className="white-text email">Log Out</span></a>
           </div>):(<div>
-            <a href="#login">
+            <a href="#login" onClick={()=>setAnimState("animatedSlideIn")}>
               <i className="material-icons logInIcon">account_circle</i>
               <span className="white-text email" onClick={()=>setUserState(true)}>Log In</span>
             </a>
           </div>)}
         </div></li>
 
-        <li><a href={userState?"#profile":"#login"}><i className="material-icons">account_circle</i>Profile</a></li>
+        <li><a href={userState?"#profile":"#login"} onClick={()=>setAnimState("animatedSlideOut")}><i className="material-icons">account_circle</i>Profile</a></li>
 
         <li><a href={userState?"#games":"#login"}><i className="material-icons">collections</i>Games</a></li>
 
