8dbaa666e84933cb9efed31d412e99a8bdb4c93f
Joaquin Valle <jv346@cornell.edu>
2020-02-27 18:19:30 -0600

    fixed merge issue

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,7 +9,6 @@ export default {
     return axios.get("/api/user/" + id);
   },
   
-<<<<<<< HEAD
   BGA:function(gameName){
     return axios.get(`https://www.boardgameatlas.com/api/search?name=${gameName}&limit=20&pretty=true&client_id=${id}`);
   },
@@ -19,22 +18,10 @@ export default {
 
   getRandom: function() {
     return axios.get(`https://www.boardgameatlas.com/api/search?order_by=popularity&limit=20&pretty=true&client_id=${id}`)
-=======
-  getName:function(gameName){
-    return axios.get(`https://www.boardgameatlas.com/api/search?name=${gameName}&limit=20&pretty=true&client_id=${BGA}`);
-  },
-  getTop: function() {
-    return axios.get(`https://www.boardgameatlas.com/api/search?order_by=popularity&limit=20&pretty=true&client_id=${BGA}`)
-  },
-
-  getRandom: function() {
-    return axios.get(`https://www.boardgameatlas.com/api/search?order_by=popularity&limit=20&pretty=true&client_id=${BGA}`)
->>>>>>> f862a2a87e27ce636922b4cd7eadfdde49434c35
   },
 
   //filter by new
   getNew: function() {
-<<<<<<< HEAD
     let res = axios.get(`https://www.boardgameatlas.com/api/search?order_by=year_published&pretty=true&client_id=${id}`)
     return res
   },
@@ -42,22 +29,11 @@ export default {
   //filter by category
   getCategory: function(category) {
     return axios.get(`https://www.boardgameatlas.com/api/search?categories&pretty=true&client_id=${id}`)
-=======
-    let res = axios.get(`https://www.boardgameatlas.com/api/search?order_by=year_published&pretty=true&client_id=${BGA}`)
-    console.log(BGA);
-    return res;
-  },
-
-  //filter by category
-  getCategories: function(category) {
-    return axios.get(`https://www.boardgameatlas.com/api/search?categories&pretty=true&client_id=${BGA}`)
->>>>>>> f862a2a87e27ce636922b4cd7eadfdde49434c35
   },
   //complexity
   getComplexity: function(complexity) {
     switch(complexity) {
       case "very-easy":
-<<<<<<< HEAD
         return axios.get(`https://www.boardgameatlas.com/api/search?lt_max_playtime=31&pretty=true&client_id=${id}`)
       case "easy":
         return axios.get(`https://www.boardgameatlas.com/api/search?mt_max_playtime=30&lt_max_playtime=61&pretty=true&client_id=${id}`)
@@ -71,23 +47,5 @@ export default {
   },
   gameId:function(id){
     return axios.get(`https://www.boardgameatlas.com/api/search?ids=${id}&pretty=true&client_id=${id}`);
-=======
-        return axios.get(`https://www.boardgameatlas.com/api/search?lt_max_playtime=31&pretty=true&client_id=${BGA}`)
-      case "easy":
-        return axios.get(`https://www.boardgameatlas.com/api/search?mt_max_playtime=30&lt_max_playtime=61&pretty=true&client_id=${BGA}`)
-      case "moderate":
-        return axios.get(`https://www.boardgameatlas.com/api/search?mt_max_playtime=61&lt_max_playtime=91&pretty=true&client_id=${BGA}`)
-      case "hard":
-        return axios.get(`https://www.boardgameatlas.com/api/search?mt_max_playtime=91&lt_max_playtime=121&pretty=true&client_id=${BGA}`)
-      case "very-hard":
-        return axios.get(`https://www.boardgameatlas.com/api/search?mt_max_playtime=121&pretty=true&client_id=${BGA}`)
-    }
-  },
-
-  getId:function(id){
-    return axios.get(`https://www.boardgameatlas.com/api/search?ids=${id}&pretty=true&client_id=${BGA}`);
-
-
->>>>>>> f862a2a87e27ce636922b4cd7eadfdde49434c35
   }
 };
