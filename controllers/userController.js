0f28c6664c7542bc4c3e560247495195b979a1c6
Omar Luna <mrebel62@gmail.com>
2020-02-27 21:08:00 -0600

    creation of JWT at login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const db = require("../models");
 const utils = require('../utils/auth')
+const jwt = require('jsonwebtoken');
 
 // Defining methods for the userController
 module.exports = {
@@ -9,9 +10,9 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   findByUsername: function(req, res) {
-    db.User.findById({ username: req.params.username })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+    return db.User.findOne({mail: req.mail})
+      .then(dbModel => {return dbModel})
+      .catch(err => console.log(err));
   },
   findAll: function(req, res) {
     db.User.find(req.query)
@@ -25,7 +26,7 @@ module.exports = {
       mail,
       password: utils.hash(password) || '1234'
     }
-    debugger;
+    // debugger;
     console.log(payload);
     db.User.create(payload)
       .then(res => {console.log(res)})
@@ -36,6 +37,24 @@ module.exports = {
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+  },
+  generateJWT: function(user){
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + 30);
+
+    console.log(`Expiration date: ${expirationDate}`);
+
+    return jwt.sign({
+      mail: user.mail,
+      exp: parseInt(expirationDate.getTime() / 1000, 10),
+    }, '$#R0ud3d-$#0R3');
+  },
+  toAuthJSON: function(user){
+    return{
+      mail: user.mail,
+      token: this.generateJWT(user)
+    }
   }
 };
 
--- routes/api/user.js
+++ routes/api/user.js
@@ -15,10 +15,10 @@ router.route("/:id")
 
 router
   .post('/signup', (req, res) => {
-    if(!req.body.email){
+    if(!req.body.mail){
       res.status(422).json({
         errors: {
-          email : 'is required'
+          mail : 'is required'
         }
       });
     }else if(!req.body.password){
@@ -29,25 +29,28 @@ router
       })
     }else{
       const {name, email, password} = req.body;
+      console.log(req.body);
       userController.create(req.body)
       // const user = req.
-        .then((res) => {
-          res.json(res);
-        })
+        // .then((res) => {
+          res.send(201);
+        // })
     }
   })
   
 
 router
-  .post('/login', (req, res) => {
-    const { user, password } = req.body;
-    db.User.findById({ username: user })
-      .then(() => {
-        if(compare(password)) {
-          res.send({message: 'holi'})  // cambiar por jwt
-        } else res.send({message: 'bye'})
-      })
-      .catch(err => res.status(404).json(err));
+  .post('/login', async (req, res) => {
+    const { mail, password } = req.body;
+    try{
+    let user = await userController.findByUsername({mail})
+      // if(compare(password)) {
+      // res.send({message: 'holi'})  // cambiar por jwt
+      res.json({ token: userController.toAuthJSON(user).token})
+      // } else res.send({message: 'bye'})
+    } catch(err){
+      console.log(err)
+    }
   })
 
 module.exports = router;
