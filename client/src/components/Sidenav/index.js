90ff0e2c1e9861c6cc7875f3830cb00e8eee0427
JoaquinValle <54565586+JoaquinValle@users.noreply.github.com>
2020-03-03 19:49:58 -0600

    added userToken in localStorage and removes token on logout

--- client/src/components/Sidenav/index.js
+++ client/src/components/Sidenav/index.js
@@ -44,7 +44,8 @@ function Nav() {
             <div className="background"></div>
             <a href="#profile"><img className="circle" src="https://randomuser.me/api/portraits/men/72.jpg" alt="profile"/></a>
             <a href="#profile"><span className="white-text name">"John Doe"</span></a>
-            <a href="#logout" onClick={()=>setUserState(1)}><span className="white-text email">Log Out</span></a>
+            <a href="#logout" onClick={()=> {
+              setUserState(1) localStorage.clear()}}><span className="white-text email">Log Out</span></a>
           </div></li>
           <li><a href="/profile"><MatIcon>account_circle</MatIcon>Profile</a></li>
           <li><a href="/games"><MatIcon>collections</MatIcon>Games</a></li>
@@ -95,6 +96,7 @@ function Nav() {
                         API.logIn({mail:emailState,password:passState})
                         .then(res=>{
                           console.log(res);
+                          localStorage.setItem('userToken', JSON.stringify(res.data.token))
                           if(res.data.message==="User not found") console.log("User not found..");
                           else{
                             setLoggedUserState(res)
